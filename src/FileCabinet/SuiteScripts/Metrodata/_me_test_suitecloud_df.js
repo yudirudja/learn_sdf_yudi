bc149be7298c8be8a2d42ab0c28186934357de3b
yudirudja <yudirudja2900@gmail.com>
2024-09-10 22:24:18 +0700

    cuma nambah comment gan

--- src/FileCabinet/SuiteScripts/Metrodata/_me_test_suitecloud_df.js
+++ src/FileCabinet/SuiteScripts/Metrodata/_me_test_suitecloud_df.js
@@ -7,6 +7,7 @@ define([], function() {
     function beforeLoad(context) {
         var rec = context.newRecord;
 
+        //test upload by SDF
         rec.setValue('custrecord_me_memo_sdf', 'Ini Testing Upload Script Dari SuiteCloud Development Framework')
     }
 
